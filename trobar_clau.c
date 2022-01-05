cb625a7c872143e94d805e93dd44cd1cde7a6a8a
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-05 12:40:40 +0200

    print

--- trobar_clau.c
+++ trobar_clau.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include "algorisme_genetic.h"
+#include "algorisme_genetic.h" 
 
-#define DEF_GENER 100 //Nombre de generacionsper per defecte
+#define DEF_GENER 100 //Nombre de generacions per defecte
 #define DEF_CROM 40 //Nombre de cromosomes per defecte
 #define DEF_PROB_M 0.05 //Probabilitat de mutació per defecte 
 #define DEF_TOUR_SEL 5 //Nombre de cromosomes al tournament selection per defecte 
@@ -15,30 +15,30 @@ int main (int argc, char* argv[]){
     int generacions = DEF_GENER;
     int crom = DEF_CROM;
     float prob_m = DEF_PROB_M;
-    int k = DEF_TOUR_SEL;
+    int tour_sel = DEF_TOUR_SEL;
     int args_tractats;
 
     if (argc > 1){
         args_tractats = 1;
 
-        while ((argc - args_tractats) > 0){
+        while ((argc - args_tractats) > 0){ //Llegir els command-line args
                
-               if (strcmp(argv[args_tractats],"-g") == 0){
+               if (strcmp(argv[args_tractats],"-g") == 0){ //Nombre de generacions 
                   
                    generacions = atoi(argv[args_tractats + 1]);
                    args_tractats += 2; 
                } 
-               else if (strcmp(argv[args_tractats],"-c") == 0){
+               else if (strcmp(argv[args_tractats],"-c") == 0){ //Nombre de cromosomes 
 
                    crom = atoi(argv[args_tractats + 1]);
                    args_tractats += 2;
                }
-               else if (strcmp(argv[args_tractats],"-prob") == 0){
+               else if (strcmp(argv[args_tractats],"-prob") == 0){ //Probabilitat de mutacio
 
                    prob_m = atof(argv[args_tractats + 1]);
                    args_tractats += 2;
                }
-               else if (strcmp(argv[args_tractats],"-k") == 0){
+               else if (strcmp(argv[args_tractats],"-ts") == 0){ //Cromosomes pel tour. sel.
 
                    k = atoi(argv[args_tractats + 1]);
                    args_tractats += 2;
@@ -47,7 +47,7 @@ int main (int argc, char* argv[]){
 
                    printf("\n ERROR: Argument desconegut\n");
                    printf("\n Les comandes reconegudes son:\n");
-                   printf("-g  generacions \n-c  cromosomes \n-prob  probabilitat de mutacio \n-k  cromosomes per el tournament selection \n");
+                   printf("-g  generacions \n-c  cromosomes \n-prob  probabilitat de mutacio \n-ts  cromosomes pel tournament selection \n");
 
                    return -1;
                }
@@ -56,7 +56,9 @@ int main (int argc, char* argv[]){
          
     }
 
-    printf("gen %d  crom %d  prob %.4f  k %d \n", generacions, crom, prob_m, k);
+    printf("\n Dades introduïdes:\n")
+
+    printf("\ngenerscions %d  cromosomes %d  probabilitat de mutacion %.4f  cromosomes pel tour.sel. %d \n", generacions, crom, prob_m, tour_sel);
  
     return 0;
 }
