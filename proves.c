4327bd2222d11ee236cd2575e1f91d40de66f2fa
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-05 23:45:31 +0200

    proves

---
+++ proves.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (){
+    int aleatori;
+    float aleatori01;
+
+    srand(time(NULL));
+
+    aleatori = rand();
+    aleatori01 = (float) aleatori / RAND_MAX;
+
+    printf ("\n %d \n", aleatori01);
+}
