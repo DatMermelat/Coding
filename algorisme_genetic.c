83167fc1fd0b74c2e9a2cc4107e150e64692801a
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-08 18:26:25 +0100

    fix

--- algorisme_genetic.c
+++ algorisme_genetic.c
@@ -7,8 +7,8 @@ void funcio_error (int ** matriu_gens, int num_fil,int num_col, int * errors){
     pirntf ("\n");
 
     for(int a  = 0; a  < num_fil; a++){
-        for(int b  = 0; b  < num_col; b++){
-            printf("%d ", matriu_gens[a]][b]);
+        for(int b  = 0; b < num_col; b++){
+            printf("%d ", matriu_gens[a][b]);
             if (b < num_col - 1){
                 printf("\n");
             }
