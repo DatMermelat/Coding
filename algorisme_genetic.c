c9cd5ed4128368dc6f430125a9ca39a312cfd88c
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-08 18:35:05 +0100

    try

--- algorisme_genetic.c
+++ algorisme_genetic.c
@@ -6,13 +6,13 @@ void funcio_error (int ** matriu_gens, int num_fil,int num_col, int * errors){
 
     for(int i = 0; i < num_fil; i++){
         suma = 0;
-        for(int j = 1; j <= num_col; j++){
-            producte = (matriu_gens[i][j]) * j * j;
+        for(int j = 0; j < num_col; j++){
+            producte = (matriu_gens[i][j]) * (j+1) * (j+1);
             printf("\n%d ", producte);
             suma += producte;
-            printf("\n%d ", suma);
+            printf("\n%d ", suma); 
             
-            if(j == num_col){
+            if(j == num_col - 1){
                 error = 1977 - suma;
                 if(error < 0){
                     error = -error; 
