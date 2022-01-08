fe5abca1b6c537163ed9b7fbbec852834c752ecf
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-08 18:14:36 +0100

    fix

--- algorisme_genetic.c
+++ algorisme_genetic.c
@@ -4,13 +4,13 @@ void funcio_error (int ** matriu_gens, int num_fil,int num_col, int * errors){
     int suma;
     int producte;
 
-    printf("\n%d\n", matriu_gens[0][0]);
-
     for(int i = 0; i < num_fil; i++){
         suma = 0;
         for(int j = 1; j <= num_col; j++){
             producte = matriu_gens[i][j] * j * j;
+            printf("\n%d ", producte);
             suma += producte;
+            printf("\n%d ");
             
             if(j == num_col){
                 error = 1977 - suma;
