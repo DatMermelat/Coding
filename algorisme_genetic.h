029b769ab1b8f9ffdda47d73ec80b6aaf77d80cf
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-09 22:55:08 +0100

    Seleccio

--- algorisme_genetic.h
+++ algorisme_genetic.h
@@ -1,1 +1,3 @@
- int funcio_error (int ** matriu_gens , int num_fil, int num_col, int * errors);
+ void funcio_error (int * errors, int ** matriu_gens , int num_fil, int num_col);
+
+ void seleccio (int * errors, int ** matriu_in, int ** matriu_out, int n_croms, int n_gens);
