bd5751ba0e3a9c49cc77f5a07a62969883ed54ce
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-10 10:01:03 +0100

    fix

--- algorisme_genetic.c
+++ algorisme_genetic.c
@@ -25,7 +25,7 @@ void funcio_error (int * errors, int ** matriu_gens, int num_fil, int num_col){
 
 void seleccio (int * errors, int ** matriu_in, int ** matriu_out, int n_croms, int n_gens){
     int posicio_min = 0;
-    int error_min = errors[posicio_min];
+    int error_min = errors[0];
 
     for (int i = 1; i < n_croms; i++){
         if (errors[i] < error_min){
