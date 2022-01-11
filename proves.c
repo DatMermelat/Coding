cbdadca4b29b1c4d7e0961b610a86b79acf2c934
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-11 18:27:00 +0100

    try

--- proves.c
+++ proves.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "algorisme_genetic.h"
 
 #define COL 5
 #define MAX 40
 
-int main (){
+int main (){}
     int test [6][COL] = {1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5};
 
+    crossover(test, 5, COL);
     printf("\n");
     for(int i = 0; i < 6; i++){
-        for(int j = 0;j  < COL; j++){
+        for(int j = 0;j   < COL; j++){
             printf("%d ", test[i][j]);
             if (j == COL - 1){
                 printf("\n");
