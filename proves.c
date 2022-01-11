d7dc63af516d68835446e61c1f2fdc2716ebc454
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-11 18:29:09 +0100

    xd

--- proves.c
+++ proves.c
@@ -5,14 +5,15 @@
 #include "algorisme_genetic.h"
 
 #define COL 5
+#define FIL 6
 #define MAX 40
 
 int main (){}
-    int test [6][COL] = {1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5};
+    int test [FIL][COL] = {1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5};
 
-    crossover(test, 5, COL);
+    crossover(test, FIL, COL);
     printf("\n");
-    for(int i = 0; i < 6; i++){
+    for(int i = 0; i < FIL; i++){
         for(int j = 0;j   < COL; j++){
             printf("%d ", test[i][j]);
             if (j == COL - 1){
