488a85f76a51bff24aa1ea35536e86c1dae2e76a
DatMermelat <97033338+DatMermelat@users.noreply.github.com>
2022-01-21 21:05:13 +0100

    pruebas xd

--- proves.c
+++ proves.c
@@ -8,17 +8,10 @@
 #define FIL 6
 #define MAX 40
 
-int main (){}
-    int test [FIL][COL] = {1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5};
+int main{
+    printf("\n Esto es una prueba para ver como de comodo es programar con una tablet que y un teclado marca blanca comprado por aliexpress\n");
 
-    crossover(test, FIL, COL);
-    printf("\n");
-    for(int i = 0; i < FIL; i++){
-        for(int j = 0;j   < COL; j++){
-            printf("%d ", test[i][j]);
-            if (j == COL - 1){
-                printf("\n");
-            }
-        }
+    for(int i = 0; i < MAX; i++){
+        printf("%d ", i);
     }
 }
